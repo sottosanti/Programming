03c389c983068359ff3df999cc4a1780367d2435
Stephen Sottosanti <stephensottosanti@pm.me>
2022-11-14 18:30:41 +0000

    golang

---
+++ Go/basics/command_line_args_2.go
@@ -0,0 +1,16 @@
+// Print command-line arguments
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+}
